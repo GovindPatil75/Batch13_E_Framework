32daa83276e3ca7355fcc84f2c7c5e97b009a93d
GovindPatil75 <mulegr75@gmail.com>
2022-10-17 21:46:52 +0530

    Test 10 added

---
+++ src/test/java/com/Test/Test_10.java
@@ -0,0 +1,14 @@
+package com.Test;
+
+import org.testng.annotations.Test;
+
+import com.Utility.BaseClass;
+
+public class Test_10 extends BaseClass{
+	
+ @Test
+ public void test10() {
+	 System.out.println("Test 10");
+ }
+
+}
