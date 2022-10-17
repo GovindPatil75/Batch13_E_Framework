3c030174642d9ef4fc697b567c89665e035acd37
GovindPatil75 <mulegr75@gmail.com>
2022-10-17 08:18:25 +0530

    Test 7 added

---
+++ src/test/java/com/Test/Test7.java
@@ -0,0 +1,14 @@
+package com.Test;
+
+import org.testng.annotations.Test;
+
+import com.Utility.BaseClass;
+
+public class Test7 extends BaseClass{
+	
+	@Test
+	public void Test_7(){
+		System.out.println("Test 7");
+	}
+
+}
