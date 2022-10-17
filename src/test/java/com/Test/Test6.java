00e993b80ecdfa828ffc5fca37a4e6ebf6979d1a
GovindPatil75 <86549802+GovindPatil75@users.noreply.github.com>
2022-10-17 08:14:37 +0530

    Update Test6.java

--- src/test/java/com/Test/Test6.java
+++ src/test/java/com/Test/Test6.java
@@ -10,5 +10,15 @@ public class Test6 extends BaseClass{
 	public void test_6() {
 		System.out.println("Test 6");
 	}
+	
+	@Test
+	public void test_6_a() {
+		System.out.println("Test 6");
+	}
+	
+	@Test
+	public void test_6_b() {
+		System.out.println("Test 6");
+	}
 
 }
